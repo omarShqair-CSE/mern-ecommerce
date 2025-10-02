d102e55262e19d1865e71367d97eba357db57276
Omar AbuShqair <omarshqair854@gmail.com>
2025-10-02 11:42:45 +0300

    Backend: user auth with register & signin, bcrypt & JWT

---
+++ routes/users.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/UserSchema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res
+      .status(400)
+      .json({ message: "Name and Email and password are required" });
+  }
+
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: "User Already Exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  let token = jwt.sign({ email, id: newUser._id }, process.env.SECRET_KEY, {
+    expiresIn: "2w",
+  });
+  res
+    .status(201)
+    .json({ message: "User register successfully", user: newUser, token });
+});
+
+router.post("/signin", async (req, res) => {
+  console.log("Signin route hit");
+  const { password, email } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Email  and password are required" });
+  }
+
+  let user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    let token = jwt.sign({ email, id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1w",
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User Signin successfully", user, token });
+  } else {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const connectDB = require("./config/db");
 connectDB();
 app.use(cors());
 
+app.use(express.json());
+app.use("/users", require("./routes/users"));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
