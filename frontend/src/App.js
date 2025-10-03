ada4c999dd96539326e1c1e31957b74ac1d5c61a
Omar AbuShqair <omarshqair854@gmail.com>
2025-10-03 22:37:48 +0300

    Connect frontend components: integrate Navbar, Hero carousel, Highlight features into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import Hero from "../Components/Hero";
+import Highlight from "../Components/Highlight";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Highlight />
+    </div>
+  );
+}
+
+export default Home;
